5ea0fb3120c059b0b9115edf12e9aece032c8af9
Arad Eytan <dara9991@gmail.com>
2025-04-04 17:18:29 +0300

    disable sandbox

--- investBot.js
+++ investBot.js
@@ -22,7 +22,14 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',  // Prevents crashes from /dev/shm issues
+            '--disable-gpu',  // Disables GPU acceleration
+            '--single-process', // Runs everything in one process (reduces RAM usage)
+            '--no-zygote'  // Prevents multiple processes from spawning
+        ]
     }
 });
 
